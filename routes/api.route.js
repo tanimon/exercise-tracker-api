a536ba86f7725e7acd4c899b239f4ec5f82f068b
tanimon <8575113+tanimon@users.noreply.github.com>
2021-08-14 10:59:31 +0900

    Add the endpoint for creating exercises

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const controller = require('../controllers/user.controller');
+const userController = require('../controllers/user.controller');
+const exerciseController = require('../controllers/exercise.controller');
 
 const router = express.Router();
 
-router.post('/users', controller.createUser);
+router.post('/users', userController.createUser);
+router.post('/users/:_id/exercises', exerciseController.createExercise);
 
 module.exports = router;
