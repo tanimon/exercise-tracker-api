bbac1ecfd0441f4b685a64f52278b36f4d1e289c
tanimon <8575113+tanimon@users.noreply.github.com>
2021-08-14 10:55:10 +0900

    Add Exercise model

---
+++ repository/mongo/models/exercise.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: false },
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+const createExercise = async (userId, description, duration, date) => {
+  const exercise = await Exercise.create({
+    user: userId,
+    description,
+    duration,
+    date,
+  });
+  const populatedExercise = await exercise.execPopulate('user');
+
+  return {
+    _id: populatedExercise._id,
+    username: populatedExercise.user.username,
+    description: populatedExercise.description,
+    duration: populatedExercise.duration,
+    date: populatedExercise.date,
+  };
+};
+
+exports.queries = { createExercise };
--- repository/mongo/mongo.repository.js
+++ repository/mongo/mongo.repository.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const userModel = require('./models/user.model');
+const exerciseModel = require('./models/exercise.model');
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -10,4 +11,5 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 module.exports = {
   user: { ...userModel.queries },
+  exercise: { ...exerciseModel.queries },
 };
