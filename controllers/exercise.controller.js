3c1a6bac6e705590c766fecb8e3b255e3f6af36a
tanimon <8575113+tanimon@users.noreply.github.com>
2021-08-18 10:05:18 +0900

    Add a method to process a request to get exercise logs

--- controllers/exercise.controller.js
+++ controllers/exercise.controller.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const exerciseService = require('./../services/exercise.service');
 
 const createExercise = async (req, res) => {
@@ -27,6 +28,36 @@ const createExercise = async (req, res) => {
   res.json(exercise);
 };
 
+const getExercisesByUserId = async (req, res) => {
+  const userId = req.params._id;
+  const from = parseDate(req.query.from);
+  const to = parseDate(req.query.to);
+  const limit = parseInt(req.query.limit);
+
+  req.log.info(
+    `[exercise.controller] Getting exercises for the user having ID: ${userId}`
+  );
+
+  const exercises = await exerciseService.getExercisesByUserId(
+    userId,
+    from,
+    to,
+    limit
+  );
+
+  req.log.info(
+    `[exercise.controller] Got exercises: ${JSON.stringify(exercises)}`
+  );
+
+  res.json(exercises);
+};
+
+const parseDate = (dateStr) => {
+  const maybeDate = moment.utc(dateStr, 'YYYY-MM-DD');
+  return maybeDate.isValid() ? maybeDate.toDate() : undefined;
+};
+
 module.exports = {
   createExercise,
+  getExercisesByUserId,
 };
